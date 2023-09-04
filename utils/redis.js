33939a18792ab7372bc5b3b35779c3464b07f43d
mumbuamutuku <mumbuamutuku@gmail.com>
2023-09-04 09:42:01 +0000

    fixelint

--- utils/redis.js
+++ utils/redis.js
@@ -1,54 +1,53 @@
 const redis = require('redis');
 
 class RedisClient {
-        constructor() {
-                this.client = redis.createClient();
+  constructor() {
+    this.client = redis.createClient();
 
-                this.client.on('error', (error) => {
-                        console.error('Redis Error:', error);
-                });
-        }
+    this.client.on('error', (error) => {
+      console.error('Redis Error:', error);
+    });
+  }
 
-        isAlive() {
-                return this.client.connected;
-        }
+  isAlive() {
+    return this.client.connected;
+  }
 
-        async get(key) {
-                return new Promise((resolve, reject) => {
-                        this.client.get(key, (error, result) => {
-                                if (error) {
-                                        reject(error);
-                                }
-                                else {
-                                        resolve(result);
-                                }
-                        });
-                });
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
         }
+      });
+    });
+  }
 
-        async set(key, value, duration) {
-                return new Promise((resolve, reject) => {
-                        this.client.setex(key, duration, value, (error, result) => {
-                                if (error) {
-                                        reject(error);
-                                } else {
-                                        resolve(result);
-                                }
-                        });
-		});
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
         }
+      });
+    });
+  }
 
-        async del(key) {
-                return new Promise((resolve, reject) => {
-                        this.client.del(key, (error, result) => {
-                                if (error) {
-                                        reject(error);
-                                } else {
-                                        resolve(result);
-                                }
-                        });
-                });
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
         }
+      });
+    });
+  }
 }
 
 // Create and export an instance of RedisClient
