4c810ab28d82161bba1415ed14903e05c3160da7
mumbuamutuku <mumbuamutuku@gmail.com>
2023-09-04 22:41:37 +0300

    una

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,16 @@
 // routes/index.js
 const express = require('express');
-
 const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
+const AuthController = require('../controllers/AuthController'); // Import the AuthController
 
 // Define routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-//UserController routes
 router.post('/users', UsersController.postNew);
 
-//authentication endpoints
+// Add new endpoints for authentication
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
